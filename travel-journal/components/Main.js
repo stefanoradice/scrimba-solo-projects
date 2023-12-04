82f43d6ad52422faf744c43e1bcb621fede6f934
Stefano Radice <stefano.radice@3dee.it>
2023-12-04 22:40:06 +0100

    travel journal

---
+++ travel-journal/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './Card'
+import data from '../data'
+
+export default function Main() {
+    const cards = data.map(item => {
+        return <Card key={item.id} {...item} />
+    })
+    return (
+        <main className="travel-journal">
+            {cards}
+        </main>
+    )
+}
