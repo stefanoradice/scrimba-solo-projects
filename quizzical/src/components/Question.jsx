3f9ff5e64f396eeed53515868ebcf7c6fd0aea05
Stefano Radice <sradice87@gmail.com>
2024-02-21 17:22:41 +0100

    update answers

--- quizzical/src/components/Question.jsx
+++ quizzical/src/components/Question.jsx
@@ -1,7 +1,7 @@
 export const Question = (props) => {
-    const { answer, question, incorrect_answers, correct_answer } = props
+    const {title, answer, question, incorrect_answers, correct_answer } = props
+
     const allAnswers = incorrect_answers
-    console.log(props)
     if (!incorrect_answers.includes(correct_answer)) {
         allAnswers.splice(Math.floor(Math.random() * (incorrect_answers.length + 1)), 0, correct_answer)
     }
@@ -10,7 +10,7 @@ export const Question = (props) => {
             <h2>{question}</h2>
             <div className='answers__list'>
                 {allAnswers.map((currentAnswer, index) => {
-                    let selected = currentAnswer === answer ? 'selected' : ''
+                    let selected = question === title && currentAnswer === answer ? 'selected' : ''
                     return <button key={index} className={`answers__item ${selected}`} onClick={() => props.selectAnswer(currentAnswer, question)}>{currentAnswer}</button>
                 })}
             </div>
--- quizzical/src/components/Quiz.jsx
+++ quizzical/src/components/Quiz.jsx
@@ -5,12 +5,13 @@ const Quiz = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [questions, setQuestions] = useState([])
+  const [answers, setAnswers] = useState([])
   const [countAnswer, setCountAnswer] = useState(0)
 
   useEffect(() => {
     (async () => {
       try {
-        const response = await axios.get(`https://opentdb.com/api.php?amount=5&type=multiple&token=37ae13f4659c857513aec1daf299a2a959c6b4078a008af5c998c796d5e724da`)
+        const response = await axios.get(`https://opentdb.com/api.php?amount=5&type=multiple`)
         setQuestions(response.data.results)
       } catch (error) {
         setIsError(true);
@@ -21,29 +22,50 @@ const Quiz = () => {
 
   const selectAnswer = (answer, questionTitle) => {
     setCountAnswer(prevState => prevState < 5 ? prevState + 1 : prevState)
-    setQuestions(prevState => {
+    setAnswers(prevState => {
+      console.log(prevState.length)
+      if (prevState.length <= 0) return [{ title: questionTitle, answer }]
+      let answer_exists = false
       const newState = prevState.map(question => {
-        if (question.question === questionTitle) {
-          return { ...question, answer: answer }
+        if (question.title === questionTitle) {
+          answer_exists = true
+          return { ...question, answer }
         }
         return question
       })
+      if (!answer_exists) return [...prevState, { title: questionTitle, answer }]
       return newState
     })
+    /*     setQuestions(prevState => {
+          const newState = prevState.map(question => {
+            if (question.question === questionTitle) {
+              return { ...question, answer: answer }
+            }
+            return question
+          })
+          return newState
+        }) */
   }
-
+  useEffect(() => {
+    console.log(answers)
+  }, [answers])
+  const checkAnsers = () => {
+    questions.forEach(element => {
+      console.log(element, element.incorrect_answers.includes(element.answer))
+    });
+  }
   return (
     <>
       {!isLoading && isError ? <p className='notices'>An error occured</p> : (isLoading ? <p className='notices'>loading...</p> : (
         <div className='questions__list'>
           {
-            Array.isArray(questions) && questions.length > 0 ? questions.map((question, index) => <Question key={index} selectAnswer={selectAnswer} {...question} />) : ''
+            Array.isArray(questions) && questions.length > 0 ? questions.map((question, index) => <Question key={index} selectAnswer={selectAnswer} {...answers} {...question} />) : ''
           }
         </div>
       ))}
 
       <div className='actions'>
-        {countAnswer === 5 && <button className='check'>Check answers</button>}
+        {countAnswer === 5 && <button className='check' id='check' onClick={checkAnsers}>Check answers</button>}
       </div>
     </>
   )
