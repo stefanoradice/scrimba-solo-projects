239c32faf0d25539419913b61d17c23a5171a298
Stefano Radice <sradice87@gmail.com>
2024-02-23 22:53:15 +0100

    finishing

--- quizzical/src/components/Question.jsx
+++ quizzical/src/components/Question.jsx
@@ -1,17 +1,25 @@
 export const Question = (props) => {
-    const {title, answer, question, incorrect_answers, correct_answer } = props
-
+    const { answer, question, incorrect_answers, correct_answer } = props
     const allAnswers = incorrect_answers
     if (!incorrect_answers.includes(correct_answer)) {
         allAnswers.splice(Math.floor(Math.random() * (incorrect_answers.length + 1)), 0, correct_answer)
     }
+    let statusAnswer = ''
     return (
         <div className='questions__item'>
             <h2>{question}</h2>
             <div className='answers__list'>
                 {allAnswers.map((currentAnswer, index) => {
-                    let selected = question === title && currentAnswer === answer ? 'selected' : ''
-                    return <button key={index} className={`answers__item ${selected}`} onClick={() => props.selectAnswer(currentAnswer, question)}>{currentAnswer}</button>
+                    let selected = currentAnswer === answer ? 'selected' : ''
+                    if (props.verify) {
+                        selected += ' verify'
+                        if (currentAnswer === correct_answer) {
+                            statusAnswer = 'correct'
+                        } else {
+                            statusAnswer = 'wrong'
+                        }
+                    }
+                    return <button key={index} className={`answers__item ${selected} ${statusAnswer}`} onClick={() => props.selectAnswer(currentAnswer, question)}>{currentAnswer}</button>
                 })}
             </div>
         </div>
--- quizzical/src/components/Quiz.jsx
+++ quizzical/src/components/Quiz.jsx
@@ -5,68 +5,69 @@ const Quiz = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [questions, setQuestions] = useState([])
-  const [answers, setAnswers] = useState([])
+  const [verify, setVerify] = useState(false)
   const [countAnswer, setCountAnswer] = useState(0)
+  const [results, setResults] = useState(0)
+  const [start, setStart] = useState(false)
 
   useEffect(() => {
     (async () => {
       try {
-        const response = await axios.get(`https://opentdb.com/api.php?amount=5&type=multiple`)
+        setIsLoading(true)
+        const response = await axios.get(`https://opentdb.com/api.php?amount=5&type=multiple&difficulty=easy`)
         setQuestions(response.data.results)
       } catch (error) {
         setIsError(true);
       }
       setIsLoading(false);
     })()
-  }, [])
+  }, [start])
 
   const selectAnswer = (answer, questionTitle) => {
     setCountAnswer(prevState => prevState < 5 ? prevState + 1 : prevState)
-    setAnswers(prevState => {
-      console.log(prevState.length)
-      if (prevState.length <= 0) return [{ title: questionTitle, answer }]
-      let answer_exists = false
+    setQuestions(prevState => {
       const newState = prevState.map(question => {
-        if (question.title === questionTitle) {
-          answer_exists = true
-          return { ...question, answer }
+        if (question.question === questionTitle) {
+          return { ...question, answer: answer }
         }
         return question
       })
-      if (!answer_exists) return [...prevState, { title: questionTitle, answer }]
       return newState
     })
-    /*     setQuestions(prevState => {
-          const newState = prevState.map(question => {
-            if (question.question === questionTitle) {
-              return { ...question, answer: answer }
-            }
-            return question
-          })
-          return newState
-        }) */
   }
-  useEffect(() => {
-    console.log(answers)
-  }, [answers])
-  const checkAnsers = () => {
-    questions.forEach(element => {
-      console.log(element, element.incorrect_answers.includes(element.answer))
-    });
+
+  const restart = () => {
+    setStart(prevState => !prevState)
+    setVerify(false)
+    setCountAnswer(0)
   }
+
+  const checkAnswers = () => {
+    setVerify(true)
+    let correctAnswers = 0
+    questions.map(question => {
+      if (question.answer === question.correct_answer) {
+        correctAnswers++
+      }
+    })
+    setResults(correctAnswers)
+  }
+
   return (
     <>
       {!isLoading && isError ? <p className='notices'>An error occured</p> : (isLoading ? <p className='notices'>loading...</p> : (
-        <div className='questions__list'>
-          {
-            Array.isArray(questions) && questions.length > 0 ? questions.map((question, index) => <Question key={index} selectAnswer={selectAnswer} {...answers} {...question} />) : ''
-          }
-        </div>
+        <>
+          <div className='questions__list'>
+            {
+              Array.isArray(questions) && questions.length > 0 ? questions.map((question, index) => <Question key={index} selectAnswer={selectAnswer} verify={verify} {...question} />) : ''
+            }
+          </div>
+          <div className='actions'>
+            {countAnswer === 5 && !verify && <button className='check' id='check' onClick={checkAnswers}>Check answers</button>}
+            {verify && <div className='results'>Yout scored {results}/5 answers <button className='restart' id='restart' onClick={() => restart()}>Play again</button></div>}
+          </div>
+        </>
       ))}
-
-      <div className='actions'>
-        {countAnswer === 5 && <button className='check' id='check' onClick={checkAnsers}>Check answers</button>}
-      </div>
     </>
   )
 }
